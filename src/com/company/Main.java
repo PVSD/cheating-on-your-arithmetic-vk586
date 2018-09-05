0b98d317a1d53ea0640793fde1cd2d9c635ae226
Valentina Kononenko <valentina@kononenko.net>
2018-09-05 17:27:41 -0400

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int a1 = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+	    int a2 = (179 + 21 + 10) / 7 + 181;
+        int a3 = 10389 * 56 * 11 + 2246;
+
+	    System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + a1);
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + a2);
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + a3);
     }
 }
